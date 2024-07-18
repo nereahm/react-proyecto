4ceb7dac57b5f1605c31ae3341c70fe4dd26ce94
Nerea <heblesn@gmail.com>
2024-07-18 18:32:24 +0200

    Add files via upload

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -19,12 +19,24 @@ function Contacto() {
           </p>
           <p className="item__infoContacto">
             <span className="etiqueta__infoContacto">Dirección:</span> Calle
-            Falsa, 123, Sevilla, España
+            Feria, 12345, Sevilla, España
           </p>
           <p className="item__infoContacto">
             <span className="etiqueta__infoContacto">Horario de atención:</span>{" "}
             Lunes a Viernes de 9:00 a 18:00
           </p>
+        </div>
+        <div className="mapaContenedor">
+        <iframe
+            className="mapa"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.6076882844613!2d-5.994256225602029!3d37.39910907208249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126c07ab602a41%3A0x91a0020a7c0796d9!2sC.%20Feria%2C%20Casco%20Antiguo%2C%20Sevilla!5e0!3m2!1ses!2ses!4v1721319962822!5m2!1ses!2ses"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
     </div>
